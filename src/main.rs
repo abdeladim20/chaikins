df67b06561bc0a2e6cc504db13a64f43fad61ea8
ajabbour <abdeladimjabbouri0@gmail.com>
2025-08-06 11:21:25 +0100

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use macroquad::prelude::*;  
+use macroquad::prelude::*;
 
 #[macroquad::main("Chaikin's Algo")]
 async fn main() {
     let mut control_points = Vec::new();
     let mut display_points = Vec::new();
     let mut is_animating = false;
+    let mut animation_step = 0;
+    let mut last_update_time = 0.0;
+
     loop {
         clear_background(BLACK);
         if is_mouse_button_pressed(MouseButton::Left) && !is_animating {
@@ -12,51 +15,67 @@ async fn main() {
         }
         if is_key_pressed(KeyCode::Enter) && control_points.len() >= 2 {
             is_animating = true;
+            animation_step = 0;
+            last_update_time = get_time();
             display_points = control_points.clone();
         }
+        
         for x in &control_points {
             draw_circle(x.0, x.1, 5.0, WHITE);
         }
-        if is_animating {
-            for i in 0..control_points.len()-1 {
-                draw_line(control_points[i].0, control_points[i].1, control_points[i+1].0, control_points[i+1].1, 1.0, WHITE);
-            }
-            chaiken(&display_points);
-        }
 
+        if is_animating {
+            if get_time() - last_update_time > 1.0 {
+                if animation_step < 7 {
+                    display_points = chaiken(&display_points);
+                    animation_step += 1;
+                }else {
+                    animation_step = 0;
+                    display_points = control_points.clone();
+                }
+                last_update_time = get_time();
+            }
+
+            if display_points.len() > 1 {
+                for points_pair in display_points.windows(2) {
+                    let p1 = points_pair[0];
+                    let p2 = points_pair[1];
+                    draw_line(p1.0, p1.1, p2.0, p2.1, 2.0, BLUE);
+                }
+            }
+            let text = format!("Step: {}", animation_step + 1);
+            draw_text(&text, 20.0, 40.0, 40.0, YELLOW);
+        }
         next_frame().await
     }
 }
 
-pub fn chaiken(points: &mut Vec<f32, f32>){
 
-/* 
-    function chaikin(p0, p1) {
-  const percent = 0.25;
-  const dx = p1.x - p0.x;
-  const dy = p1.y - p0.y;
-  return [
-    {
-      x: p0.x + dx * percent,
-      y: p0.y + dy * percent,
-    },
-    {
-      x: p0.x + dx * (1 - percent),
-      y: p0.y + dy * (1 - percent),
-    },
-  ];
-}
-*/
-// let percent = 0.25;
-for i in 0..points.len()-2 {
+pub fn chaiken(points: &[(f32, f32)]) -> Vec<(f32, f32)> {
+    if points.len() < 2 {
+        return points.to_vec();
+    }
 
-    let dx = points[i+1].0 - points[i].0;
-    let dy = points[i+1].1 - points[i].1;
-    // points.push(dx *0.25, dy * 0.25);
-    // points.push(dx *0.75, dy * 0.75);
-    points.insert(i+1, (dx *0.75, dy * 0.75));
-    points.insert(i+1, (dx *0.25, dy * 0.25));
-}
+    let mut new_points = Vec::new();
+    new_points.push(points[0]);
 
+    for points_pair in points.windows(2) {
+        let p0 = points_pair[0];
+        let p1 = points_pair[1];
 
+        let distance_x = p1.0 - p0.0;
+        let distance_y = p1.1 - p0.1;
+
+        let q0_x = p0.0 + distance_x * 0.25;
+        let q0_y = p0.1 + distance_y * 0.25;
+
+        let q1_x = p0.0 + distance_x * 0.75;
+        let q1_y = p0.1 + distance_y * 0.75;
+
+        new_points.push((q0_x, q0_y));
+        new_points.push((q1_x, q1_y));
+    }
+    new_points.push(points[points.len() - 1]);
+
+    new_points
 }
